4d974c41f333edc5df5ffe1a70b03fd2852f1665
deeksha-sn <deekshasn18@gmail.com>
2026-04-12 10:51:15 +0530

    updated maze_solver.h with code

--- backend/maze_solver.h
+++ backend/maze_solver.h
@@ -31,6 +31,7 @@ int is_cell_valid(const Maze *maze, int row, int col);
 int read_maze(FILE *input, Maze *maze);
 void print_maze(const Maze *maze);
 int find_special_points(const Maze *maze, Point *start, Point *end);
+
 SolveResult solve_maze_bfs(Maze *maze);
 SolveResult solve_maze_dfs(Maze *maze);
 
